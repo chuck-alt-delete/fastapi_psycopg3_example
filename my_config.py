6676ed587fde0f331dd0dcaba3dfb150dce096d8
chuck-alt-delete <chuck@materialize.com>
2023-05-23 16:36:11 -0700

    use traditional postgres connection string to abstract cluster selection

--- my_config.py
+++ my_config.py
@@ -9,8 +9,11 @@ config = dotenv_values(path_to_env)
 # Set Materialize cluster name
 CLUSTER = config["CLUSTER"] if config["CLUSTER"] else "default"
 
-# Create Data Source Name (DSN) string
-MY_DSN_STRING = f'user={config["MZ_USER"]} password={config["MZ_PASSWORD"]} host={config["MZ_HOST"]} port={config["MZ_PORT"]} dbname={config["MZ_DB"]} sslmode=require'
+# Replace the @ symbol of your Materialize user email with %40 for the postgres connection string
+config["MZ_USER"] = config["MZ_USER"].replace('@','%40')
+
+# Create Data Source Name (DSN) string, including Materialize cluster the query will run on
+MY_DSN_STRING = f'''postgresql://{config["MZ_USER"]}:{config["MZ_PASSWORD"]}@{config["MZ_HOST"]}:{config["MZ_PORT"]}/{config["MZ_DB"]}?options=--cluster%3D{CLUSTER}&sslmode=require'''
 
 if __name__=="__main__":
     print(config)
--- my_queries.py
+++ my_queries.py
@@ -2,7 +2,7 @@ from psycopg_pool import AsyncConnectionPool
 from pydantic import BaseModel
 from psycopg.rows import class_row
 
-from my_config import CLUSTER
+# from my_config import CLUSTER
 
 class MyClass(BaseModel):
     a: int
@@ -11,7 +11,6 @@ class MyClass(BaseModel):
 async def my_query(pool: AsyncConnectionPool) :
     async with pool.connection() as conn:
         async with conn.cursor(row_factory=class_row(MyClass)) as cur:
-            await cur.execute(f"SET CLUSTER = {CLUSTER}")
             await cur.execute("SELECT a, b FROM my_collection")
             rows = await cur.fetchall()
             return {"data": rows}
